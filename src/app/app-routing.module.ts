e6ca8930feb03b39caefc622ca00f1dee4195ac2
Isabella Campos <c0655850@vale.com>
2024-01-04 01:47:34 -0300

    feat: start routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'deck-list', pathMatch: 'full' },
+  // { path: 'deck-list', loadChildren: () => import('./').then(m => m.)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import {
+	IgxIconModule,
+	IgxNavbarModule,
+	IgxButtonModule,
+  IgxCardModule,
+ } from "igniteui-angular";
+import { DeckListComponent } from './pages/deck-list/deck-list.component';
+import { DeckCreateComponent } from './pages/deck-create/deck-create.component';
+import { CardComponent } from './components/card/card.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CardService } from './services/card.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DeckListComponent,
+    DeckCreateComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IgxNavbarModule,
+    IgxIconModule,
+    IgxButtonModule,
+    IgxCardModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
