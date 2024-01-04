4bdc7cc01ceb7d31fb040ea221355fa86a7bf67d
Isabella Campos <c0655850@vale.com>
2024-01-04 11:14:19 -0300

    feat: create supertype service

---
+++ src/app/services/supertypes.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SupertypesService } from './supertypes.service';
+
+describe('SupertypesService', () => {
+  let service: SupertypesService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SupertypesService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/services/supertypes.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { ResponseSupertypes } from '../models/supertypes.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupertypesService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllSupertypes(): Observable<ResponseSupertypes> {
+    const url = "https://api.pokemontcg.io/v2/supertypes";
+    return this.http.get<ResponseSupertypes>(url).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error);
+    return throwError(() => 'Something went wrong; Please try again later.');
+  }
+}
