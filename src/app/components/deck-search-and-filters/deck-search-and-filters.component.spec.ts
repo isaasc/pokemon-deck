9d1521c1b5bec708b6cd01d75171b819efe5189f
Isabella Campos <c0655850@vale.com>
2024-01-04 11:27:17 -0300

    feat: create deck search filters component

---
+++ src/app/components/deck-search-and-filters/deck-search-and-filters.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeckSearchAndFiltersComponent } from './deck-search-and-filters.component';
+
+describe('DeckSearchAndFiltersComponent', () => {
+  let component: DeckSearchAndFiltersComponent;
+  let fixture: ComponentFixture<DeckSearchAndFiltersComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DeckSearchAndFiltersComponent]
+    });
+    fixture = TestBed.createComponent(DeckSearchAndFiltersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/deck-search-and-filters/deck-search-and-filters.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ResponseSupertypes } from 'src/app/models/supertypes.interface';
+import { ResponseTypes } from 'src/app/models/types.interface';
+import { SupertypesService } from 'src/app/services/supertypes.service';
+import { TypesService } from 'src/app/services/types.service';
+
+@Component({
+  selector: 'app-deck-search-and-filters',
+  templateUrl: './deck-search-and-filters.component.html',
+  styleUrls: ['./deck-search-and-filters.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DeckSearchAndFiltersComponent implements OnInit {
+
+  constructor(private typesService: TypesService, private supertypesService: SupertypesService){}
+
+  cardType$!: Observable<ResponseTypes>;
+  cardSupertype$!: Observable<ResponseSupertypes>;
+
+  ngOnInit(): void {
+    this.cardType$ = this.typesService.getAllTypes();
+    this.cardSupertype$ = this.supertypesService.getAllSupertypes();
+  }
+}
