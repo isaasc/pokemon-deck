605272f62949177cfb0411c845a5932b42010073
Isabella Campos <c0655850@vale.com>
2024-01-04 11:15:52 -0300

    feat: add change detection on push

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardComponent {
 
--- src/app/pages/deck-list/deck-list.component.ts
+++ src/app/pages/deck-list/deck-list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-deck-list',
   templateUrl: './deck-list.component.html',
-  styleUrls: ['./deck-list.component.scss']
+  styleUrls: ['./deck-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DeckListComponent {
 
