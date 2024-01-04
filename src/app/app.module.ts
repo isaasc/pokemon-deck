a9a023d501139bcf51fdd8b060b04c3447fd7686
Isabella Campos <c0655850@vale.com>
2024-01-04 11:27:37 -0300

    feat: add new components on app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -9,6 +10,8 @@ import {
 	IgxNavbarModule,
 	IgxButtonModule,
   IgxCardModule,
+  IgxSelectModule,
+	IgxInputGroupModule
  } from "igniteui-angular";
 import { DeckListComponent } from './pages/deck-list/deck-list.component';
 import { DeckCreateComponent } from './pages/deck-create/deck-create.component';
@@ -16,6 +19,9 @@ import { CardComponent } from './components/card/card.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CardService } from './services/card.service';
 import { HttpClientModule } from '@angular/common/http';
+import { DeckDetailsComponent } from './components/deck-details/deck-details.component';
+import { DeckSearchAndFiltersComponent } from './components/deck-search-and-filters/deck-search-and-filters.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
   declarations: [
@@ -24,16 +30,22 @@ import { HttpClientModule } from '@angular/common/http';
     DeckListComponent,
     DeckCreateComponent,
     CardComponent,
+    DeckDetailsComponent,
+    DeckSearchAndFiltersComponent,
+    CardListComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     IgxNavbarModule,
     IgxIconModule,
     IgxButtonModule,
     IgxCardModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    IgxSelectModule,
+    IgxInputGroupModule
   ],
   providers: [CardService],
   bootstrap: [AppComponent]
--- src/app/pages/deck-create/deck-create.component.ts
+++ src/app/pages/deck-create/deck-create.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PokemonCard } from 'src/app/models/pokemon-card.interface';
+import { CardService } from 'src/app/services/card.service';
 
 @Component({
   selector: 'app-deck-create',
   templateUrl: './deck-create.component.html',
-  styleUrls: ['./deck-create.component.scss']
+  styleUrls: ['./deck-create.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DeckCreateComponent {
+export class DeckCreateComponent implements OnInit{
+  pokemon$!: Observable<PokemonCard[]>;
 
+  constructor(private cardService: CardService) {}
+
+  ngOnInit(): void {
+      this.pokemon$ = this.cardService.getAllCards();
+  }
 }
