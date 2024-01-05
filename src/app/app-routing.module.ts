98711226e68cbab472402c9603487d0b007a6555
Isabella Campos <c0655850@vale.com>
2024-01-05 12:11:31 -0300

    feat: start routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeckCreateComponent } from './pages/deck-create/deck-create.component';
+import { DeckListComponent } from './pages/deck-list/deck-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'deck-list', pathMatch: 'full' },
-  // { path: 'deck-list', loadChildren: () => import('./').then(m => m.)}
+  // { path: 'list', loadComponent: () => import('./pages/deck-list').then((x) => x.DeckListComponent) },
+  { path: '', loadComponent: () => DeckListComponent },
+  { path: 'create', loadComponent: () => DeckCreateComponent },
 ];
 
 @NgModule({
