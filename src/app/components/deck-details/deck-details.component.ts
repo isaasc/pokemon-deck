5aabc6c9360d4d8a5e02148c89876549e8b78e5c
Isabella Campos <c0655850@vale.com>
2024-01-05 18:16:16 -0300

    refactor: improve deck details layout

--- src/app/components/deck-details/deck-details.component.ts
+++ src/app/components/deck-details/deck-details.component.ts
@@ -1,36 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { NgIf } from '@angular/common';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IGX_DIALOG_DIRECTIVES, IGX_EXPANSION_PANEL_DIRECTIVES, IgxInputGroupModule } from 'igniteui-angular';
+import { Observable, Subject, takeUntil } from 'rxjs';
 import { Supertypes } from 'src/app/models/enums/supertype.enum';
 import { PokemonCard } from 'src/app/models/pokemon-card.interface';
-import { DeckDetailsService } from './deck-details.service';
 import { CardListComponent } from '../card-list/card-list.component';
-import { NgIf } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { IgxExpansionPanelComponent, IgxExpansionPanelHeaderComponent, IgxExpansionPanelTitleDirective, IgxInputGroupComponent, IgxInputDirective, IgxLabelDirective, IgxButtonDirective, IgxRippleDirective, IgxExpansionPanelBodyComponent } from 'igniteui-angular';
+import { DeckDetailsService } from './deck-details.service';
 
 @Component({
-    selector: 'app-deck-details',
-    templateUrl: './deck-details.component.html',
-    styleUrls: ['./deck-details.component.scss'],
-    standalone: true,
-    imports: [
-        IgxExpansionPanelComponent,
-        IgxExpansionPanelHeaderComponent,
-        IgxExpansionPanelTitleDirective,
-        ReactiveFormsModule,
-        IgxInputGroupComponent,
-        IgxInputDirective,
-        IgxLabelDirective,
-        IgxButtonDirective,
-        IgxRippleDirective,
-        NgIf,
-        IgxExpansionPanelBodyComponent,
-        CardListComponent,
-    ],
+  selector: 'app-deck-details',
+  templateUrl: './deck-details.component.html',
+  styleUrls: ['./deck-details.component.scss'],
+  standalone: true,
+  imports: [
+    IGX_EXPANSION_PANEL_DIRECTIVES,
+    ReactiveFormsModule,
+    NgIf,
+    CardListComponent,
+    IGX_DIALOG_DIRECTIVES,
+    IgxInputGroupModule,
+  ],
 })
-export class DeckDetailsComponent implements OnInit {
+export class DeckDetailsComponent implements OnInit, OnDestroy {
   @Input() isDetailsMode: boolean = false;
   deckCards$!: Observable<PokemonCard[]>;
+  subscription = new Subject();
   numberOfPokemons: number = 0;
   numberOfTrainers: number = 0;
   numberOfEnergies: number = 0;
@@ -41,7 +36,7 @@ export class DeckDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.deckCards$.subscribe(cards => {
+    this.deckCards$.pipe(takeUntil(this.subscription)).subscribe(cards => {
       if (cards.length === 0) {
         return;
       }
@@ -50,5 +45,9 @@ export class DeckDetailsComponent implements OnInit {
       this.numberOfEnergies = cards.filter(card => card.supertype === Supertypes.ENERGY).length;
     });
     this.deckDetailsService.getDeckCards() ? 'oi' : 'tchau';
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.complete();
   }
 }
