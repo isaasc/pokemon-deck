779758e4dece8a79cc6c19d5e4f2fd8a3453fede
Isabella Campos <c0655850@vale.com>
2024-01-04 21:36:43 -0300

    feat: add input on card list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,31 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import {
-  IgxIconModule,
-  IgxNavbarModule,
   IgxButtonModule,
   IgxCardModule,
-  IgxSelectModule,
+  IgxForOfModule,
+  IgxIconModule,
   IgxInputGroupModule,
+  IgxListModule,
+  IgxNavbarModule,
+  IgxPaginatorModule,
+  IgxRippleModule,
+  IgxSelectModule,
 } from 'igniteui-angular';
-import { DeckListComponent } from './pages/deck-list/deck-list.component';
-import { DeckCreateComponent } from './pages/deck-create/deck-create.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 import { CardComponent } from './components/card/card.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { CardService } from './services/card.service';
-import { HttpClientModule } from '@angular/common/http';
 import { DeckDetailsComponent } from './components/deck-details/deck-details.component';
 import { DeckSearchAndFiltersComponent } from './components/deck-search-and-filters/deck-search-and-filters.component';
-import { CardListComponent } from './components/card-list/card-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DeckCreateComponent } from './pages/deck-create/deck-create.component';
+import { DeckListComponent } from './pages/deck-list/deck-list.component';
+import { CardService } from './services/card.service';
 
 @NgModule({
   declarations: [
@@ -46,6 +50,10 @@ import { CardListComponent } from './components/card-list/card-list.component';
     HttpClientModule,
     IgxSelectModule,
     IgxInputGroupModule,
+    IgxRippleModule,
+    IgxForOfModule,
+    IgxListModule,
+    IgxPaginatorModule,
   ],
   providers: [CardService],
   bootstrap: [AppComponent],
--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { PokemonCard } from 'src/app/models/pokemon-card.interface';
+import { CardService } from 'src/app/services/card.service';
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CardListComponent {}
+export class CardListComponent {
+  @Input() allCards!: PokemonCard[];
+
+  constructor(private cardService: CardService) {}
+}
