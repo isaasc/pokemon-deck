c6a4d39ecccb555d5b75b74f6a48503f56215767
Isabella Campos <c0655850@vale.com>
2024-01-05 18:17:41 -0300

    refactor: layout adjusts

--- src/app/components/mini-card/mini-card.component.ts
+++ src/app/components/mini-card/mini-card.component.ts
@@ -1,25 +1,29 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
-import { PokemonCard } from 'src/app/models/pokemon-card.interface';
-import { IgxCardComponent, IgxLayoutDirective, IgxFlexDirective, IgxCardHeaderComponent, IgxCardHeaderTitleDirective, IgxCardHeaderSubtitleDirective, IgxCardMediaDirective } from 'igniteui-angular';
 import { NgIf } from '@angular/common';
+import {
+  IgxCardComponent,
+  IgxCardHeaderComponent,
+  IgxCardHeaderSubtitleDirective,
+  IgxCardHeaderTitleDirective,
+  IgxCardMediaDirective,
+} from 'igniteui-angular';
+import { PokemonCard } from 'src/app/models/pokemon-card.interface';
 
 @Component({
-    selector: 'app-mini-card',
-    templateUrl: './mini-card.component.html',
-    styleUrls: ['./mini-card.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [
-        NgIf,
-        IgxCardComponent,
-        IgxLayoutDirective,
-        IgxFlexDirective,
-        IgxCardHeaderComponent,
-        IgxCardHeaderTitleDirective,
-        IgxCardHeaderSubtitleDirective,
-        IgxCardMediaDirective,
-    ],
+  selector: 'app-mini-card',
+  templateUrl: './mini-card.component.html',
+  styleUrls: ['./mini-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    NgIf,
+    IgxCardComponent,
+    IgxCardHeaderComponent,
+    IgxCardHeaderTitleDirective,
+    IgxCardHeaderSubtitleDirective,
+    IgxCardMediaDirective,
+  ],
 })
 export class MiniCardComponent {
   @Input() card!: PokemonCard;
