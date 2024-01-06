b999c0550dd44bd7757cdabb51b174a208e4c512
Isabella Campos <c0655850@vale.com>
2024-01-06 19:21:56 -0300

    feat: add method to delete deck on deck-card

--- src/app/components/deck-card/deck-card.component.ts
+++ src/app/components/deck-card/deck-card.component.ts
@@ -1,16 +1,26 @@
 import { CommonModule } from '@angular/common';
 
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { IgxCardModule, IgxIconModule, IgxRippleModule } from 'igniteui-angular';
 import { Deck } from 'src/app/models/deck.interface';
+import { DeckService } from 'src/app/services/deck.service';
 
 @Component({
   selector: 'app-deck-card',
   standalone: true,
-  imports: [CommonModule, IgxIconModule, IgxCardModule, IgxRippleModule],
+  imports: [CommonModule, IgxIconModule, IgxCardModule, IgxRippleModule, RouterLink],
   templateUrl: './deck-card.component.html',
   styleUrls: ['./deck-card.component.scss'],
 })
 export class DeckCardComponent {
   @Input() deck!: Deck;
+
+  constructor(private deckService: DeckService) {}
+
+  removeDeck(): void {
+    this.deckService.deleteDeck(this.deck.id!);
+  }
+
+  showDetails(): void {}
 }
