6df2f80de10fc5a4e9fcf40ee1cc5e177bbe66f0
Isabella Campos <c0655850@vale.com>
2024-01-06 02:45:28 -0300

    refactor: use deck card on deck list

--- src/app/pages/deck-list/deck-list.component.ts
+++ src/app/pages/deck-list/deck-list.component.ts
@@ -1,12 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { DeckDetailsComponent } from '../../components/deck-details/deck-details.component';
+import { NgFor, NgIf } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { DeckCardComponent } from 'src/app/components/deck-card/deck-card.component';
+import { Deck } from 'src/app/models/deck.interface';
+import { DeckService } from './../../services/deck.service';
 
 @Component({
-    selector: 'app-deck-list',
-    templateUrl: './deck-list.component.html',
-    styleUrls: ['./deck-list.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [DeckDetailsComponent],
+  selector: 'app-deck-list',
+  templateUrl: './deck-list.component.html',
+  styleUrls: ['./deck-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [DeckCardComponent, NgIf, NgFor, RouterLink],
 })
-export class DeckListComponent {}
+export class DeckListComponent {
+  decks!: Deck[];
+
+  constructor(private deckService: DeckService) {
+    this.decks = this.deckService.getDecks();
+  }
+}
