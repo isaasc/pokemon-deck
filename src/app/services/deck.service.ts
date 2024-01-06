3426f9a0c043eb2a29402b2e854add5721d72996
Isabella Campos <c0655850@vale.com>
2024-01-06 19:26:12 -0300

    feat: create observable for deckList

--- src/app/services/deck.service.ts
+++ src/app/services/deck.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
 import { Deck } from '../models/deck.interface';
 import { Supertypes } from '../models/enums/supertype.enum';
@@ -10,14 +11,22 @@ import { PokemonCard } from '../models/pokemon-card.interface';
 export class DeckService {
   private storage!: Storage;
   private key = 'card';
+  private deckListSubject = new Subject<Deck[]>();
 
   constructor() {
     this.storage = window.localStorage;
   }
 
+  getDeckListSubject(): Observable<Deck[]> {
+    return this.deckListSubject.asObservable();
+  }
+
+  getDeckById(id: string): Deck {
+    return this.getDecks().find(deck => deck.id === id)!;
+  }
+
   getDecks(): Deck[] {
     const storedDecks = this.storage.getItem(this.key);
-    console.log(storedDecks ? (JSON.parse(storedDecks) as Deck[]) : [], 'teste');
     return storedDecks ? (JSON.parse(storedDecks) as Deck[]) : [];
   }
 
@@ -32,17 +41,38 @@ export class DeckService {
     storedDecks.push(card);
 
     this.storage.setItem(this.key, JSON.stringify(storedDecks));
+    this.emitDeckList(storedDecks);
   }
 
-  deleteDeck(id: number): void {
-    this.storage.removeItem(id.toString());
+  deleteDeck(idToRemove: string): void {
+    const decks: Deck[] = this.getDecks().filter(decks => decks.id !== idToRemove);
+    this.storage.setItem(this.key, JSON.stringify(decks));
+    this.emitDeckList(decks);
   }
 
   cardNumberOfSupertype(cards: PokemonCard[], supertype: Supertypes) {
     return cards.filter(card => card.supertype === supertype).length;
   }
 
+  countUniqueTypes(cards: PokemonCard[]): number {
+    const uniqueTypes = new Set<string>();
+
+    cards.forEach(card => {
+      if (card.types && card.types.length > 0) {
+        card.types.forEach(type => {
+          uniqueTypes.add(type);
+        });
+      }
+    });
+
+    return uniqueTypes.size;
+  }
+
   isDeckIdUnique(newDeckId: string, existingDecks: Deck[]): boolean {
     return !existingDecks.some(deck => deck.id === newDeckId);
+  }
+
+  private emitDeckList(decks: Deck[]): void {
+    this.deckListSubject.next([...decks]);
   }
 }
