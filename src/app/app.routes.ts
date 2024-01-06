6a6f6f59545a94bab5daebbce973419bd9236dc2
Isabella Campos <c0655850@vale.com>
2024-01-06 02:43:48 -0300

    feat: create route details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DeckCreateComponent } from './pages/deck-create/deck-create.component';
+import { DeckDetailsComponent } from './pages/deck-details/deck-details.component';
 import { DeckListComponent } from './pages/deck-list/deck-list.component';
 
 export const APP_ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'list' },
   { path: 'list', component: DeckListComponent },
   { path: 'create', loadComponent: () => DeckCreateComponent },
+  { path: 'details/:id', loadComponent: () => DeckDetailsComponent },
 ];
